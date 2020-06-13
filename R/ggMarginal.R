bee5809cadafbe1e2da17dbbf0584b73bdc8ef26
JackEdTaylor <33028594+JackEdTaylor@users.noreply.github.com>
2020-06-13 23:23:55 +0100

    pass through arrangeGrob()

--- R/ggMarginal.R
+++ R/ggMarginal.R
@@ -209,6 +209,13 @@ print.ggExtraPlot <- function(x, newpage = grDevices::dev.interactive(), ...) {
       ggdark::dark_theme_void()
     print(x)
   } else {
-    gridExtra::grid.arrange(grid::grobTree(grid::rectGrob(gp=grid::gpar(fill="black", lwd=0)), gridExtra::grid.arrange(x)))
+    gridExtra::arrangeGrob(
+      gridExtra::grid.arrange(
+        grid::grobTree(
+          grid::rectGrob(gp=grid::gpar(fill="black", lwd=0)),
+          gridExtra::grid.arrange(x)
+        )
+      )
+    )
   }
 }
